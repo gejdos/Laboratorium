ca9432224bdf5b9c22440984d127ca9b299a1ff2
Jakub Gejdos <gejdosjakub@gmail.com>
2017-11-10 17:41:06 +0100

    interim1

---
+++ Laboratorium/Lab1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium
+{
+    partial class Lab1
+    {
+        Pristroj LaboratornyPristroj1;
+        Pristroj LaboratornyPristroj2;
+
+        private void SpustVyskum()
+        {
+
+        }
+
+    }
+}
---
+++ Laboratorium/Lab2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium
+{
+    partial class Lab2
+    {
+
+        class VeduciLaboratoria
+        {
+            private string meno, priezvisko;
+
+            public VeduciLaboratoria(string meno, string priezvisko)
+            {
+                this.meno = meno;
+                this.priezvisko = priezvisko;
+            }
+
+
+            public void VypisMeno()
+            {
+                Console.WriteLine("Meno: {0}\nPriezvisko: {1}", meno, priezvisko);
+            }
+
+
+        }
+
+    }
+}
